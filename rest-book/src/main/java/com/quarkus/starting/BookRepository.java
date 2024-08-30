98d7443f2a2ac5c4f6c5019fb48e085d263738e4
Neimat1 <neimat.soliman.ismail@gmail.com>
2024-08-31 01:52:35 +0300

    add configurations

--- rest-book/src/main/java/com/quarkus/starting/BookRepository.java
+++ rest-book/src/main/java/com/quarkus/starting/BookRepository.java
@@ -1,6 +1,7 @@
 package com.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,9 +9,12 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "book.genre", defaultValue = "drama")
+    String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "Love Next Door", "Korean Author", 2024, "empty"),
+                new Book(1, "Love Next Door", "Korean Author", 2024, genre),
                 new Book(2, "Alchemy Of Souls", "Korean Author", 2019, "empty"),
                 new Book(2, "Marry My Husband", "Korean Author", 2023, "empty"),
                 new Book(3, "Queen Of Tears", "Korean Author", 2024, "empty")
