b0225864aea33aaaaf18e4789b74192a59a46f46
Neimat1 <neimat.soliman.ismail@gmail.com>
2024-08-31 00:52:13 +0300

    add unit tests

--- rest-book/src/test/java/com/quarkus/starting/BookResourceTest.java
+++ rest-book/src/test/java/com/quarkus/starting/BookResourceTest.java
@@ -1,6 +1,10 @@
 package com.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
@@ -8,9 +12,26 @@ import static org.hamcrest.Matchers.is;
 @QuarkusTest
 public class BookResourceTest {
 
-    public void testHelloEndpoint(){
-        given().when().
-                get("/api/book").then().
-                statusCode(200).body(is("Hello Quarkus!"));
+    @Test
+    public void testGetAllBooksEndpoint() {
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .when().get("/api/books")
+                .then().statusCode(200).body("size()", is(4));
     }
+
+    @Test
+    public void testCountAllBooksEndpoint() {
+        given().header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
+                .when().get("/api/books/count")
+                .then().statusCode(200).body(is("4"));
+    }
+
+
+    @Test
+    public void testGetBookByIdEndpoint() {
+        given().header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).pathParam("id", 1)
+                .when().get("/api/books/{id}")
+                .then().statusCode(200).body("title", is("Love Next Door"));
+    }
+
 }
